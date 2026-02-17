c3cd37449034ecd941b506b6625d29689dcfb06a
qnoxs <florianbress@gmail.com>
2026-02-18 00:19:51 +0100

    postgres issus fixed

--- machines/nas/modules/containers/postgres.nix
+++ machines/nas/modules/containers/postgres.nix
@@ -6,7 +6,7 @@ in
 {
   virtualisation.oci-containers.containers.postgres = {
     image = "postgres:17";
-    ports = [ "5432:5432" ];
+    ports = [ "5433:5432" ];
     volumes = [
       "${cfg.storage.containerDataPath}/postgres:/var/lib/postgresql/data"
     ];
@@ -20,9 +20,23 @@ in
     "d ${cfg.storage.containerDataPath}/postgres 0700 999 999 -"
   ];
 
-  networking.firewall.allowedTCPPorts = [ 5432 ];
+  networking.firewall.allowedTCPPorts = [ 5433 ];
 
-  # Clan vars generator for Postgres password
+  # Clan vars generators for Postgres credentials
+  clan.core.vars.generators.postgres-user = {
+    prompts.value = {
+      description = "PostgreSQL superuser username";
+      type = "line";
+    };
+    files.secret = {
+      secret = true;
+      neededFor = "services";
+    };
+    script = ''
+      cat $prompts/value > $out/secret
+    '';
+  };
+
   clan.core.vars.generators.postgres-password = {
     prompts.value = {
       description = "PostgreSQL superuser password";
@@ -46,10 +60,16 @@ in
       Type = "oneshot";
       RemainAfterExit = true;
     };
-    script = ''
+    script = let
+      userPath = config.clan.core.vars.generators.postgres-user.files.secret.path;
+      passPath = config.clan.core.vars.generators.postgres-password.files.secret.path;
+    in ''
       mkdir -p /var/lib/postgres
+      PG_USER=$(cat ${userPath})
       cat > /var/lib/postgres/secrets.env << EOF
-      POSTGRES_PASSWORD=$(cat ${config.clan.core.vars.generators.postgres-password.files.secret.path})
+      POSTGRES_USER=$PG_USER
+      POSTGRES_PASSWORD=$(cat ${passPath})
+      POSTGRES_DB=$PG_USER
       EOF
       chmod 600 /var/lib/postgres/secrets.env
     '';
